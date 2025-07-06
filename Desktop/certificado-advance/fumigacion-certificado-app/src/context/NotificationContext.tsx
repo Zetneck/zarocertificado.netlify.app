ed29b242320d0c151806d2f8f68d018b79f3db81
Zetneck <fil-th@outlook.com>
2025-07-06 11:54:03 -0600

    Delete Desktop directory

--- Desktop/certificado-advance/fumigacion-certificado-app/src/context/NotificationContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-import type { AlertColor } from '@mui/material';
-
-export interface NotificationContextType {
-  showNotification: (message: string, severity?: AlertColor, duration?: number) => void;
-}
-
-export const NotificationContext = createContext<NotificationContextType | undefined>(undefined);
--- Desktop/certificado-advance/fumigacion-certificado-app/src/themes/ThemeToggle.tsx
+++
@@ -1,9 +0,0 @@
-import { IconButton } from '@mui/material';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import LightModeIcon from '@mui/icons-material/LightMode';
-
-export const ThemeToggle = ({ mode, toggleMode }: { mode: string, toggleMode: () => void }) => (
-  <IconButton onClick={toggleMode} color="inherit">
-    {mode === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
-  </IconButton>
-);
